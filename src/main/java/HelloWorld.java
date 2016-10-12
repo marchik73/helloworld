b4958cf2f06646819b1abe42158da013ba40ade3
root <root@kali>
2016-10-12 17:46:40 +0300

    commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,8 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
+	String myTimeString = "test error";
+
 	System.out.println("hello world");
 	System.out.println("hello world");
 	System.out.println("hello world");
