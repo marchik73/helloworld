cd0a16fb118ac5e57d2bacd86059f45471b7c7b4
root <root@kali>
2016-10-14 01:02:46 +0300

    commit for github

---
+++ .sonar/batch-report/issues-2.pb
@@ -0,0 +1,2 @@
+O
+squidS1228=Add a 'package-info.java' file to document the 'java' package 
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
 	String myTimeString = "test error";
-	String myTimeString = "test error";
+	String myTimeString1 = "test error";
 
 	System.out.println("hello world");
 	System.out.println("hello world");
